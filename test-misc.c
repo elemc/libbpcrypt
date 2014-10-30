7f5bcb59e6ffd8d21e1c71624ff112d06eb09f02
elemc <me@elemc.name>
2014-10-31 01:21:55 +0300

    split tests

--- test-misc.c
+++ test-misc.c
@@ -16,6 +16,7 @@ int main ( int argc, char *argv[] ) {
     char *buf = "Тестовое сообщение";
     int len = strlen(buf);
     char *hex_hash_template="5e3d5c58fa308ad3fc40c94d4f9c79d2";
+    char *hex_template="d0a2d0b5d181d182d0bed0b2d0bed0b520d181d0bed0bed0b1d189d0b5d0bdd0b8d0b5";
 
     bp_buffer_t *test_buf = calloc( sizeof( bp_buffer_t ), len+1 );
     bp_size_t test_len = len;
@@ -27,13 +28,24 @@ int main ( int argc, char *argv[] ) {
     bp_buffer_t *hex = BP_to_hex( hash, MD5_DIGEST_LENGTH, &hex_len );
 
     if ( memcmp( hex, hex_hash_template, hex_len ) == 0 )
-        printf("Hash and HEX works fine! OK.\n");
+        printf("Hash works fine! OK.\n");
     else {
-        printf("Hash or HEX works ugly! Fail.\n");
+        printf("Hash works ugly! Fail.\n");
         return_result = 100;
     }
 
-    printf("[DEBUG] For string \"%s\" hex is \"%s\"\n", test_buf, hex );
+    bp_size_t hex_b_len;
+    bp_buffer_t *hex_buffer = BP_to_hex( test_buf, test_len, &hex_b_len );
+
+    if ( memcmp( hex_template, hex_buffer, hex_b_len ) == 0 )
+        printf("HEX works fine! OK.\n");
+    else {
+        printf("HEX works ugly! Fail.\n");
+        return_result = 200;
+    }
+
+    printf("[DEBUG] For string \"%s\" hex of hash is \"%s\"\n", test_buf, hex );
+    printf("[DEBUG] For string \"%s\" hex is \"%s\"\n", test_buf, hex_buffer );
 
     free( hash );
     free( hex );
