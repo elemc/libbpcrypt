9dbb21e06a777d2e22c1d08263cd3d05282b0168
elemc <me@elemc.name>
2014-10-31 19:10:57 +0300

    Added memcmp tests and error hooks for stage3

--- test-old-crypt.c
+++ test-old-crypt.c
@@ -46,49 +46,85 @@ bp_buffer_t *read_file( bp_buffer_t *filename, bp_size_t *filesize ) {
 
 int main( int argc, char *argv[] ) {
 
+    int return_result = 0;
+
     char *test_buffer = "Тестовый блок";
     bp_size_t test_buffer_size = strlen( test_buffer );
 
     char *test_key = "3510";
     bp_size_t test_key_size = strlen( test_key );
 
+    char *hex_crypt_test = "e53b332042f2e5203f562f1e09e89113975dc9a898c094b1bdd992353a16061a674e061f7269195e57355527dab69f4b";
+
+    // Stage 1. Crypt.
     bp_size_t test1_size;
     bp_buffer_t *test1 = old_encrypt_buffer( (bp_buffer_t *)test_buffer, test_buffer_size, (bp_buffer_t *)test_key, test_key_size, &test1_size );
-    printf("[DEBUG] test=%s(%d)\n", test1, test1_size);
+    //printf("[DEBUG] test=%s(%d)\n", test1, test1_size);
 
     bp_size_t hex_test1_size;
     bp_buffer_t *hex_test1 = BP_to_hex( test1, test1_size, &hex_test1_size );
-    printf("Crypt buffer HEX: \"%s\"\n", hex_test1 ); 
+    //printf("[DEBUG] crypt buffer HEX: \"%s\"\n", hex_test1 ); 
 
+    if ( memcmp( hex_test1, hex_crypt_test, hex_test1_size ) == 0 )
+        printf("Stage 1. Crypt test success. OK\n");
+    else {
+        printf("Stage 1. Crypt test failed. Fail\n");
+        return_result = 100;
+    }
+
+    // Stage 2. Decrypt
     bp_size_t detest1_size;
     bp_buffer_t *detest1 = old_decrypt_buffer( (bp_buffer_t *)test1, test1_size, (bp_buffer_t *)test_key, test_key_size, &detest1_size );
-    printf("[DEBUG] detest=%s(%d)\n", detest1, detest1_size);
+    //printf("[DEBUG] detest=%s(%d)\n", detest1, detest1_size);
 
+    if ( memcmp( detest1, test_buffer, detest1_size ) == 0 )
+        printf("Stage 2. Decrypt test success. OK\n");
+    else {
+        printf("Stage 2. Decrypt test failed. Fail\n");
+        return_result = 200;
+    }
+ 
+    free( test1 );
+    free( hex_test1 );
+    free( detest1 );
+
+    // Stage 3. File decrypt
     // test for decrypt file
     char *home_dir = getenv("HOME");
     char *basket_file = "/.basketpwd/default.cxml";
     bp_buffer_t *file_path = calloc( sizeof( bp_buffer_t ), strlen(home_dir) + strlen(basket_file) + 1 );
     sprintf( (char *)file_path, "%s%s", home_dir, basket_file );
 
-    printf("[DEBUG] File for reading: %s\n", file_path );
+    //printf("[DEBUG] File for reading: %s\n", file_path );
 
     bp_size_t file_size;
     bp_buffer_t *file_content = read_file( file_path, &file_size );
-
+    bp_buffer_t *clean_file_content = NULL;
     bp_size_t cfc_size;
-    bp_buffer_t *clean_file_content = old_decrypt_buffer( file_content, file_size, (bp_buffer_t *)test_key, test_key_size, &cfc_size );
+    
+    if ( file_content == NULL ) {
+        printf( "Stage 3. File decrypt failed. %s (%s)\n", strerror(errno), file_path );
+        return_result = 300;
+    }
+    else {
+        clean_file_content = old_decrypt_buffer( file_content, file_size, (bp_buffer_t *)test_key, test_key_size, &cfc_size );
+    }
 
     int wd;
     wd = open( "default.xml", O_WRONLY | O_CREAT, 0644 );
-    write( wd, clean_file_content, cfc_size );
-    close( wd );
-
-    free( test1 );
-    free( hex_test1 );
-    free( detest1 );
+    if ( wd == -1 ) {
+        printf( "Stage 3. File decrypt failed. %s (default.xml)\n", strerror(errno) );
+        return_result = 400;
+    }
+    else if ( clean_file_content != NULL ){
+        write( wd, clean_file_content, cfc_size );
+        close( wd );
+        printf("Stage 3. File decrypt waiting. Please check \"default.xml\" file...\n");
+    }
+    
     free( file_path );
     free( file_content );
     free( clean_file_content );
 
-    return 0;
+    return return_result;
 }
