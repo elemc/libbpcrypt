b8e15541a7944af5d1081adc472465833f91fadc
Алексей <a.panov@maximatelecom.ru>
2016-09-27 12:27:53 +0300

    Fix for linux 1 to 0

--- test-tree.c
+++ test-tree.c
@@ -5,7 +5,7 @@
 /* ------------------------------------ */
 
 /* Name: (test-tree)
-   Description: 
+   Description:
 */
 
 #include <libbpcrypt/tree.h>
@@ -66,7 +66,7 @@ void my_malloc_init() {
     zone->free      = &my_free;
     zone->calloc    = &my_calloc;
     zone->free_definite_size    = &my_free_ds;
- 
+
     if (mprotect(zone, getpagesize(), PROT_READ) != 0) {
         perror("mprotect failed");
         abort();
@@ -102,7 +102,7 @@ void *calloc (size_t num, size_t size) {
 
 void free ( void *ptr ) {
     void *caller = __builtin_return_address(0);
-    if (malloc_hook_active) 
+    if (malloc_hook_active)
         my_free_hook(ptr, caller);
     else
         __libc_free(ptr);
@@ -137,42 +137,42 @@ void my_free_hook ( void *ptr, void *caller ) {
 }
 
 #elif __FreeBSD__
-#include <dlfcn.h> 
-/*HOOKS IMPLEMENTATION*/ 
-#define PARENT_FRAME 0x01 
+#include <dlfcn.h>
+/*HOOKS IMPLEMENTATION*/
+#define PARENT_FRAME 0x01
 
 #define MEMORY_CLEAN 1
 
-void *(*__malloc)(size_t size); 
-void *(*__malloc_hook)(size_t size, const void *caller); 
-void *(*__cur_malloc_hook)(size_t size, const void *caller); 
+void *(*__malloc)(size_t size);
+void *(*__malloc_hook)(size_t size, const void *caller);
+void *(*__cur_malloc_hook)(size_t size, const void *caller);
 
-void *(*__calloc)(size_t num, size_t size); 
-void *(*__calloc_hook)(size_t num, size_t size, const void *caller); 
-void *(*__cur_calloc_hook)(size_t num, size_t size, const void *caller); 
+void *(*__calloc)(size_t num, size_t size);
+void *(*__calloc_hook)(size_t num, size_t size, const void *caller);
+void *(*__cur_calloc_hook)(size_t num, size_t size, const void *caller);
 
-void (*__free)(void *ptr); 
-void (*__free_hook)(void *ptr, const void *caller); 
-void (*__cur_free_hook)(void *ptr, const void *caller); 
+void (*__free)(void *ptr);
+void (*__free_hook)(void *ptr, const void *caller);
+void (*__cur_free_hook)(void *ptr, const void *caller);
 
-void *malloc(size_t size){ 
-    const void *caller = __builtin_return_address(PARENT_FRAME); 
-    __malloc = dlsym(RTLD_NEXT, "malloc"); 
-                 
-    if(__malloc_hook) 
-        return __malloc_hook(size, caller); 
+void *malloc(size_t size){
+    const void *caller = __builtin_return_address(PARENT_FRAME);
+    __malloc = dlsym(RTLD_NEXT, "malloc");
 
-    return __malloc(size); 
-} 
+    if(__malloc_hook)
+        return __malloc_hook(size, caller);
+
+    return __malloc(size);
+}
 
 void *calloc(size_t num, size_t size) {
-    const void *caller = __builtin_return_address(PARENT_FRAME); 
-    __calloc = dlsym(RTLD_NEXT, "calloc"); 
-             
-    if(__calloc_hook) 
-        return __calloc_hook(num, size, caller); 
+    const void *caller = __builtin_return_address(PARENT_FRAME);
+    __calloc = dlsym(RTLD_NEXT, "calloc");
 
-    return __calloc(num, size); 
+    if(__calloc_hook)
+        return __calloc_hook(num, size, caller);
+
+    return __calloc(num, size);
 }
 
 void free( void *ptr ) {
@@ -185,30 +185,30 @@ void free( void *ptr ) {
         __free( ptr );
 }
 
-/*IMPLEMENTATION ENDS AND USER APP CODE BEGINS*/ 
+/*IMPLEMENTATION ENDS AND USER APP CODE BEGINS*/
 
-void *my_malloc_hook(size_t size, const void *caller){ 
-    /*protecting our hook's printf from endless recursion*/ 
-    __cur_malloc_hook = __malloc_hook; 
-    __malloc_hook = NULL; 
-    
+void *my_malloc_hook(size_t size, const void *caller){
+    /*protecting our hook's printf from endless recursion*/
+    __cur_malloc_hook = __malloc_hook;
+    __malloc_hook = NULL;
+
     memory_count += 1;
 
-    __malloc_hook = __cur_malloc_hook; 
-    return __malloc(size); 
-} 
+    __malloc_hook = __cur_malloc_hook;
+    return __malloc(size);
+}
 
-void *my_calloc_hook(size_t num, size_t size, const void *caller){ 
-    /*protecting our hook's printf from endless recursion*/ 
-    __cur_calloc_hook = __calloc_hook; 
-    __calloc_hook = NULL; 
-             
-    memory_count += 1;             
+void *my_calloc_hook(size_t num, size_t size, const void *caller){
+    /*protecting our hook's printf from endless recursion*/
+    __cur_calloc_hook = __calloc_hook;
+    __calloc_hook = NULL;
 
-    __calloc_hook = __cur_calloc_hook; 
-                     
-    return __calloc(num, size); 
-} 
+    memory_count += 1;
+
+    __calloc_hook = __cur_calloc_hook;
+
+    return __calloc(num, size);
+}
 
 void my_free_hook( void *ptr, const void *caller ) {
     __cur_free_hook = __free_hook;
@@ -247,7 +247,7 @@ int main( int argc, char *argv[] ) {
 #ifdef __APPLE__
     my_malloc_init();
 #elif __linux
-    malloc_hook_active = 1;
+    malloc_hook_active = 0;
 #elif __FreeBSD__
     my_init_hooks();
 #endif
@@ -270,13 +270,13 @@ int main( int argc, char *argv[] ) {
     printf("\nMemory allocated: %llu\n", memory_count);
     BPTree_Final( tree );
     printf("Memory count after final: %llu\n", memory_count);
-    if ( memory_count == MEMORY_CLEAN ) 
+    if ( memory_count == MEMORY_CLEAN )
         printf("Memory is clean. OK!\n");
 #elif __FreeBSD__
     printf("\nMemory allocated: %lu\n", memory_count);
     BPTree_Final( tree );
     printf("Memory count after final: %lu\n", memory_count);
-    if ( memory_count == MEMORY_CLEAN ) 
+    if ( memory_count == MEMORY_CLEAN )
         printf("Memory is clean. OK!\n");
 #else
     BPTree_Final( tree );
